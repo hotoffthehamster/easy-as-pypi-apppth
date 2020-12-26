33fcab2ba587a1f32152be80562a7f1470d0c70f
landonb <landonb@users.noreply.github.com>
2020-12-26 12:51:04 -0600

    Tests: Add tmpdir XDG environ fixture.

--- easy_as_pypi_apppth/tests/appdirs_mock.py
+++ easy_as_pypi_apppth/tests/appdirs_mock.py
@@ -32,27 +32,66 @@ from unittest import mock
 from easy_as_pypi_apppth.app_dirs_with_mkdir import AppDirsWithMkdir
 from easy_as_pypi_apppth.expand_and_mkdirs import must_ensure_directory_exists
 
+__all__ = (
+    'tmp_appdirs',
+    'xdg_appdirs',
+    # PRIVATE:
+    #  'ensure_appdir_exists',
+)
 
-def mocker_patch_app_dirs(mocker, tmpdir, which):
-    prop_name = '{}_dir'.format(which)
-    tmp_appdir = os.path.join(tmpdir.mkdir(which).strpath, 'easy-as-pypi-apppth')
-    # Because mocking property, which is wrapped by @mkdir_side_effect, do same,
-    # albeit preemptively. (lb): Seriously, such a smelly side effect.
-    must_ensure_directory_exists(tmp_appdir)
-    pkg_path = 'easy_as_pypi_apppth.app_dirs_with_mkdir.AppDirsWithMkdir'
-    target = '{}.{}'.format(pkg_path, prop_name)
-    mocker.patch(target, new_callable=mock.PropertyMock(return_value=tmp_appdir))
+# XDG_* mapping as seen in appdirs source:
+#  $(virtualenvwrapper_get_site_packages_dir)/appdirs.py
+APPDIRS_DIRS = (
+    ( 'user_data', 'XDG_DATA_HOME', ),
+    ( 'site_data', 'XDG_DATA_DIRS', ),
+    ( 'user_config', 'XDG_CONFIG_HOME', ),
+    ( 'site_config', 'XDG_CONFIG_DIRS', ),
+    ( 'user_cache', 'XDG_CACHE_HOME', ),
+    ( 'user_state', 'XDG_STATE_HOME', ),
+    ( 'user_log', '', ),  # {XDG_CACHE_HOME}/log
+)
 
 
 @pytest.fixture
 def tmp_appdirs(mocker, tmpdir):
-    """Provide mocked version specific user dirs using a tmpdir."""
-    mocker_patch_app_dirs(mocker, tmpdir, 'user_data')
-    mocker_patch_app_dirs(mocker, tmpdir, 'site_data')
-    mocker_patch_app_dirs(mocker, tmpdir, 'user_config')
-    mocker_patch_app_dirs(mocker, tmpdir, 'site_config')
-    mocker_patch_app_dirs(mocker, tmpdir, 'user_cache')
-    mocker_patch_app_dirs(mocker, tmpdir, 'user_state')
-    mocker_patch_app_dirs(mocker, tmpdir, 'user_log')
-    return AppDirsWithMkdir()
+    """Provide mocked AppDirs whose paths share a common base tmpdir."""
+    def _tmp_appdirs():
+        for appdir_dir_def in APPDIRS_DIRS:
+            mocker_patch_app_dirs(mocker, tmpdir, appdir_dir_def[0])
+
+        return AppDirsWithMkdir()
+
+    def mocker_patch_app_dirs(mocker, tmpdir, appdir_dir):
+        tmp_appdir = ensure_appdir_exists(tmpdir, appdir_dir)
+        pkg_path = 'easy_as_pypi_apppth.app_dirs_with_mkdir.AppDirsWithMkdir'
+        prop_name = '{}_dir'.format(appdir_dir)
+        target = '{}.{}'.format(pkg_path, prop_name)
+        mocker.patch(target, new_callable=mock.PropertyMock(return_value=tmp_appdir))
+
+    return _tmp_appdirs()
+
+
+@pytest.fixture
+def xdg_appdirs(mocker, tmpdir):
+    """Provide mocked AppDirs whose paths use tmpdir via XDG_* environs."""
+    def _xdg_appdirs():
+        for appdir_dir, xdg_environ in APPDIRS_DIRS:
+            environ_patch_app_dirs(mocker, tmpdir, appdir_dir, xdg_environ)
+
+    def environ_patch_app_dirs(mocker, tmpdir, appdir_dir, xdg_environ):
+        if not xdg_environ:
+            return
+
+        tmp_appdir = ensure_appdir_exists(tmpdir, appdir_dir)
+        mocker.patch.dict(os.environ, { xdg_environ: tmp_appdir })
+
+    _xdg_appdirs()
+
+
+def ensure_appdir_exists(tmpdir, appdir_dir):
+    tmp_appdir = os.path.join(tmpdir.mkdir(appdir_dir).strpath, 'easy-as-pypi-apppth')
+    # Because mocking property, which is wrapped by @mkdir_side_effect, do same,
+    # albeit preemptively. (lb): Seriously, such a smelly side effect.
+    must_ensure_directory_exists(tmp_appdir)
+    return tmp_appdir
 
